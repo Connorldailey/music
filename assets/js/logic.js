b87d78dae0bfe9d419a3aaab297cc124622233d8
Connor Dailey <connorldailey@gmail.com>
2024-09-15 15:09:21 -0400

    Trivia game working

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -4,3 +4,81 @@ gameplayEl.addEventListener('click', function() {
     console.log("here");
     window.location = "trivia.html";
 });
+
+// Renders the stats table section on the page (requires sorted statsObject)
+const renderStatsTable = function(statsObject) {
+    // Reference the stats-table div
+    const statsTableEl = document.querySelector('#stats-table');
+    // Clear the content of the stats-table
+    statsTableEl.innerHTML = "";
+    // Check if there are any stats in local storage
+    if (!statsObject || statsObject.length === 0) {
+        statsTableEl.setAttribute('style', 'border: none;')
+        const emptyTableMessage = document.createElement('p');
+        emptyTableMessage.textContent = "Nothing to display yet. Play to place.";
+        statsTableEl.appendChild(emptyTableMessage);
+        return;
+    } 
+    // Create the table header
+    const tableHeaderEl = document.createElement('tr');
+    const positionHeaderEl = document.createElement('th');
+    positionHeaderEl.textContent = "Position";
+    tableHeaderEl.appendChild(positionHeaderEl);
+    const usernameHeaaderEl = document.createElement('th');
+    usernameHeaaderEl.textContent = "Username";
+    tableHeaderEl.appendChild(usernameHeaaderEl);
+    const scoreHeaderEl = document.createElement('th');
+    scoreHeaderEl.textContent = "Score";
+    tableHeaderEl.appendChild(scoreHeaderEl);
+    const timeHeaderEl = document.createElement('th');
+    timeHeaderEl.textContent = "Average Time";
+    tableHeaderEl.appendChild(timeHeaderEl);
+    const dateHeaderEl = document.createElement('th');
+    dateHeaderEl.textContent = "Date";
+    tableHeaderEl.appendChild(dateHeaderEl);
+    // Append the table header to the stats table
+    statsTableEl.appendChild(tableHeaderEl);
+
+    // Sort the statsObject from highest to lowest score
+    statsObject.sort((a, b) => b.score - a.score);
+
+    for (let i = 0; i < 10; i++) {
+        if (i < statsObject.length) {
+            // Construct each table row
+            const tableRowEl = document.createElement('tr');
+            const positionEl = document.createElement('td');
+            positionEl.textContent = i + 1;
+            tableRowEl.appendChild(positionEl);
+            const usernameEl = document.createElement('td');
+            usernameEl.textContent = statsObject[i].username;
+            tableRowEl.appendChild(usernameEl);
+            const scoreEl = document.createElement('td');
+            scoreEl.textContent = statsObject[i].score;
+            tableRowEl.appendChild(scoreEl);
+            const timeEl = document.createElement('td');
+            timeEl.textContent = statsObject[i].avgTime;
+            tableRowEl.appendChild(timeEl);
+            const dateEl = document.createElement('td');
+            dateEl.textContent = statsObject[i].date;
+            tableRowEl.appendChild(dateEl);
+            // Append the row to the table
+            statsTableEl.appendChild(tableRowEl);
+        } else {
+            break;
+        }
+    }
+    // Add a border to the stats table element
+    statsTableEl.setAttribute('style', 'border: 1px solid black;');
+}
+
+// Reads local storage and returns the statsObject
+const readStatsFromStorage = function() {
+    const statsObject = localStorage.getItem('statsObject');
+    if (statsObject && statsObject.length > 0) {
+        return JSON.parse(statsObject);
+    } else {
+        return [];
+    }
+}
+
+renderStatsTable(readStatsFromStorage());
--- assets/js/trivia.js
+++ assets/js/trivia.js
@@ -1,29 +1,37 @@
+// Reference the trivia-section
 const triviaSectionEl = document.querySelector('#trivia-section');
 
-const formModuleEl = document.querySelector('#username-module');
-const usernameFormEl = document.querySelector('#username-form');
+// Create an object to store gameplay data
+let gameplayObject = {
+    username: "",
+    score: 0,
+    numQuestions: 0,
+    totalTime: 0,
+    date: new Date().toJSON().slice(0, 10),
+};
 
+// Reference the username-module and username-form
+const usernameModuleEl = document.querySelector('#username-module');
+const usernameFormEl = document.querySelector('#username-form');
+// Handle username form submission
 const submitUsernameForm = function(event) {
     event.preventDefault();
     const username = usernameFormEl.username.value;
     if (!username || username === "") {
         const errorMessage = document.createElement('p');
         errorMessage.textContent = "Please enter a valid username";
-        formModuleEl.appendChild(errorMessage);
+        usernameModuleEl.appendChild(errorMessage);
         return;
     }
-    formModuleEl.setAttribute('style', 'display: none;')
-    initializeGame(username);
-}
-
-usernameFormEl.addEventListener('submit', submitUsernameForm);
-
-const initializeGame = function(username) {
+    usernameModuleEl.setAttribute('style', 'display: none;')
+    gameplayObject.username = username;
     startCountdown();
 }
+// Add an event listener to the username-form
+usernameFormEl.addEventListener('submit', submitUsernameForm);
 
 const startCountdown = function() {
-    let timeLeft = 5;
+    let timeLeft = 3;
     const countEl = document.createElement('p');
     const timeInterval = setInterval(function() {
         if (timeLeft != 0) {
@@ -38,35 +46,193 @@ const startCountdown = function() {
     }, 1000);
 }
 
+let currentQuestion = {};
+let timer;
+
 const newQuestion = function() {
+    clearRadioButtons();
+    gameplayObject.numQuestions++;
     const questionIndex = Math.floor(Math.random() * multipleChoiceQuestions.length);
     const question = multipleChoiceQuestions[questionIndex];
-    renderQuestion(question);
+    currentQuestion = question;
+    renderQuestion();
 }
 
-const renderQuestion = function(question) {
-    const triviaSectionEl = document.querySelector('#trivia-module');
-    triviaSectionEl.setAttribute('style', 'display: flex');
+const renderQuestion = function() {
+    // Display the question
+    const triviaModuleEl = document.querySelector('#trivia-module');
+    triviaModuleEl.setAttribute('style', 'display: flex');
     const promptEl = document.querySelector('#prompt');
-    promptEl.textContent = question.prompt;
+    promptEl.textContent = currentQuestion.prompt;
     const question1Label = document.querySelector('#question1Label');
-    question1Label.textContent = question.choices[0];
+    question1Label.textContent = currentQuestion.choices[0];
     const question2Label = document.querySelector('#question2Label');
-    question2Label.textContent = question.choices[1];
+    question2Label.textContent = currentQuestion.choices[1];
     const question3Label = document.querySelector('#question3Label');
-    question3Label.textContent = question.choices[2];
+    question3Label.textContent = currentQuestion.choices[2];
     const question4Label = document.querySelector('#question4Label');
-    question4Label.textContent = question.choices[3];
+    question4Label.textContent = currentQuestion.choices[3];
+    // Handle the timer
+    let timeLeft = 10;
+    const timerEl = document.querySelector('#timer');
+    timerEl.setAttribute('style', 'background-color: #66FF99;');
+    timer = setInterval(function() {
+        if (timeLeft != 0) {
+            if (timeLeft <= 3) {
+                timerEl.setAttribute('style', 'background-color: #ffcbca;');
+            }
+            timerEl.textContent = timeLeft;
+            timeLeft--;
+            gameplayObject.totalTime++;
+        } else {
+            timerEl.textContent = '';
+            clearInterval(timer);
+            displayResults(true);
+            setTimeout(() => {
+                endGame();
+            }, 3000)
+        }
+    }, 1000);
 }
 
 const triviaFormEl = document.querySelector('#trivia-form');
 
 const submitTriviaForm = function(event) {
     event.preventDefault();
+    const correctRadio = `question${currentQuestion.correctIndex + 1}Input`;
+    const timerEl = document.querySelector('#timer');
+    let correctAnswer = false;
+    if (document.getElementById(correctRadio).checked) {
+        gameplayObject.score++;
+        clearInterval(timer);
+        correctAnswer = true;
+        displayResults(correctAnswer);
+        setTimeout(() => {
+            timerEl.innerHTML = "";
+            clearMessages();
+            newQuestion();
+        }, 3000);
+    } else {
+        clearInterval(timer);
+        displayResults(correctAnswer);
+        setTimeout(() => {
+            timerEl.innerHTML = "";
+            clearMessages();
+            endGame();
+        }, 3000);
+    }
 }
 
 triviaFormEl.addEventListener('submit', submitTriviaForm);
 
+const displayResults = function(correctAnswer) {
+    const correctMessage = document.createElement('span');
+    correctMessage.textContent = "Correct";
+    correctMessage.setAttribute('style', 'color: #66FF99; margin-left: 1rem;');
+    const correctDiv = `#question${currentQuestion.correctIndex + 1}Div`;
+    const correctDivEl = document.querySelector(correctDiv);
+    correctDivEl.appendChild(correctMessage);
+    if (correctAnswer) {
+        return;
+    }
+    const incorrectMessage = document.createElement('span');
+    incorrectMessage.textContent = "Incorrect";
+    incorrectMessage.setAttribute('style', 'color: #ffcbca;  margin-left: 1rem;');
+    const radioButtons = document.querySelectorAll('input[name="question"]')
+    for (let i = 0; i < radioButtons.length; i++) {
+        if (radioButtons[i].checked) {
+            const incorrectDiv = `#question${i + 1}Div`;
+            const incorrectDivEl = document.querySelector(incorrectDiv);
+            incorrectDivEl.appendChild(incorrectMessage);
+        }
+    }
+}
+
+const clearMessages = function() {
+    const messageElements = document.querySelectorAll('#trivia-form span');
+    for (message of messageElements) {
+        message.remove();
+    }
+}
+
+const clearRadioButtons = function() {
+    const radioButtons = document.querySelectorAll('input[name="question"]');
+    for (button of radioButtons) {
+        button.checked = false;
+    }
+}
+
+const endGame = function() {
+    const triviaModuleEl = document.querySelector('#trivia-module');
+    triviaModuleEl.setAttribute('style', 'display: none;');
+    const endgameModuleEl = document.querySelector('#endgame-module');
+    endgameModuleEl.setAttribute('style', 'display: flex;');
+    const unformattedAvgTime = gameplayObject.totalTime / gameplayObject.numQuestions;
+    const avgTime = Math.round(unformattedAvgTime);
+    let formattedAvgTime;
+    if (avgTime === 1) {
+        formattedAvgTime = `${Math.round(unformattedAvgTime)} second`;
+    } else {
+        formattedAvgTime = `${Math.round(unformattedAvgTime)} seconds`;
+    }
+    const statObject = {
+        username: gameplayObject.username,
+        score: gameplayObject.score,
+        avgTime: formattedAvgTime,
+        date: gameplayObject.date
+    }
+    const usernameEl = document.querySelector('#user');
+    usernameEl.textContent = statObject.username;
+    const scoreEl = document.querySelector('#score');
+    scoreEl.textContent = statObject.score;
+    const avgTimeEl = document.querySelector('#avgTime');
+    avgTimeEl.textContent = statObject.avgTime;
+    const dateEl = document.querySelector('#date');
+    dateEl.textContent = statObject.date;
+    updateStatsInStorage(statObject);
+}
+
+const restartButton = document.querySelector('#restart-button');
+const restartGame = function(username) {
+    gameplayObject = {
+        username: username,
+        score: 0,
+        numQuestions: 0,
+        totalTime: 0,
+        date: new Date().toJSON().slice(0, 10),
+    };
+    const endgameModuleEl = document.querySelector('#endgame-module');
+    endgameModuleEl.setAttribute('style', 'display: none;');
+    startCountdown();
+}
+restartButton.addEventListener('click', function() {
+    restartGame(gameplayObject.username);
+});
+
+const exitButton = document.querySelector('#exit-button');
+const redirectPage = function() {
+    window.location = "index.html";
+}
+exitButton.addEventListener('click', redirectPage);
+
+// Appends a new entry to the statsObject in local storage
+const updateStatsInStorage = function(stat) {
+    const statsObject = readStatsFromStorage();
+    statsObject.push(stat);
+    localStorage.setItem('statsObject', JSON.stringify(statsObject));
+}
+
+// Reads local storage and returns the statsObject
+const readStatsFromStorage = function() {
+    const statsObject = localStorage.getItem('statsObject');
+    if (statsObject && statsObject.length > 0) {
+        return JSON.parse(statsObject);
+    } else {
+        return [];
+    }
+}
+
+// Store the list of multiple choice questions
 const multipleChoiceQuestions = [
     {
         prompt: "What is the capital of France?",
